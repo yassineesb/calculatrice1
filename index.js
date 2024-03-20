816b42006742ea571a7c9211187832f8820850e9
yassineesb <saber.yassine.i@gmail.com>
2024-03-20 12:22:12 +0100

    calcule

---
+++ index.js
@@ -0,0 +1,118 @@
+// function addition (a,b){
+//     return a + b
+// }
+
+
+// console.log(addition(1,3))
+// console.log(addition(3,5))
+
+
+// console.log("---------------------------------------------")
+
+
+// let myArr = [1,8,9,7,89,5,230,98,678,90,65,45,0,1000]
+
+
+
+
+// function findMax (arr) {
+//     let max = arr[0]
+//     for (let i = 0;i<arr.length;i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+// console.log("max======++++>",findMax(myArr))
+
+
+// function findMin(arr){
+
+//     let min = arr[0]
+
+//     for(let i = 0 ; i < arr.length ; i++){
+//         if(arr[i] < min){
+//             min = arr[i]
+//         }
+//     }
+//     return min
+// }
+
+
+// console.log("min ============++++> ;" ,findMin(myArr)) 
+
+
+4//let arr = [8,9,4,30]
+//arr.push(9)
+//arr.push(60)
+//arr.push(700000000000)
+//arr.push(666666664444444433333333)
+//console.log(arr)
+//arr.pop()
+//arr.pop()
+//arr.pop()
+//console.log(arr)
+
+//arr.unshift(5)
+//console.log(arr)
+
+//arr.shift()
+//console.log(arr)
+
+
+
+//let arr = [1,3,77,56,99,66,46,34,12,90]
+
+
+//function filtereven(arr){
+ 
+//let evenNumbers = []
+//for (let i = 0; i < arr.length;i++) {
+  //  if (arr[i] % 2 == 0){
+   //     evenNumbers.push(arr[i])
+
+    //}
+
+//}
+//return evenNumbers;
+
+//}
+
+//console.log(filtereven(arr))
+
+
+const screen = document.querySelector(".screen")
+const btn = document.querySelectorAll("div[type]")
+
+
+screen.append(btn.innerText)
+
+
+
+screen.addEventListener("click",function(){
+
+    console.log("button")
+}
+)
+
+
+
+for (let number of numbers){
+
+number.addEventListener('click')
+
+
+
+
+  screen.append(btn.innerText)
+}
+
+
+
+
+
+
+
+
+
